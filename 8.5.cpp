b873bfb6735996e9ff5b3bfb566c87059741d1ee
CRASHFIRE-666 <54841369+CRASHFIRE-666@users.noreply.github.com>
2019-12-10 16:39:53 +0300

    Create 8.5.cpp

---
+++ 8.5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x;
+    cin>>x;
+    if (x>0) {
+        if (x%2==0) {
+            cout<<"Положительное чётное число"<<endl;
+        }
+        else { cout<<"Положительное нечётное число"<<endl; }
+    }
+    else if (x==0) { cout<<"Нулевое число"<<endl; }
+    else {
+        if (x%2==0) {
+            cout<<"Отрицательное чётное число"<<endl;
+        }
+        else { cout<<"Отрицательное нечётное число"<<endl; }
+    }
+    return 0;
+}
